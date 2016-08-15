d54a9a02758aded77eb3ddf275cac93c5301fab4
Mammothrider <xushubin1992@gmail.com>
2016-08-15 16:58:38 +0900

    Add a pathfinding algorithm

--- pacman.py
+++ pacman.py
@@ -7,7 +7,7 @@ from random import *
 #by Mammothrider
 #2016.4.12
 
-_debug_ = 1
+_debug_ = 0
 
 class cBody(object):
 	def __init__(self):
@@ -33,16 +33,29 @@ class cBody(object):
 		
 class cMonsterGenerator(cBody):
 	def __init__(self):
-		#four monster is needed, their color should be red, blue, pink, yellow
+		#four monster is needed, their color should be red, cyan, pink, orange
 		#0 means ready to generate, -1 means cooldown, 1 means has been generated
-		self.monster = {"red":0, "pink":0, "blue":0, "yellow":0}
+		cBody.__init__(self)
+		self.monster = {(255, 0, 0):0, (255, 0, 255):0, (0, 255, 255):0, (255, 180,0):0}
 		
 	def generateMonster(self, monsterList):
 		num = len(monsterList)
 		if num > 3:
 			return
-		
-		
+			
+		tmp = 0
+		for m in monsterList:
+			c = m.getColor()
+			c = tuple(c)
+			if c in self.monster:
+				self.monster[c] = 1
+			
+		for c in self.monster:
+			if self.monster[c] == 0:
+				tmp = cMonster()
+				tmp.setColor(*c)
+				tmp.setPosition(*self.position)
+				monsterList.append(tmp)
 
 class cMovingBody(cBody):
 	def __init__(self):
@@ -54,20 +67,25 @@ class cMovingBody(cBody):
 		self.percentage = 0
 		self.deathFlag = 0
 		
-	def move(self):
+	def move(self, map):
 		if self.deathFlag:
 			return
 			
 		self.percentage += self.velocity
 		#from -0.5 to 0.49
-		if self.percentage > 0.5:
-			self.position[0] += self.direct[0]
-			self.position[1] += self.direct[1]
+		while self.percentage > 0.5:
+			next = [self.position[0] + self.direct[0], \
+					self.position[1] + self.direct[1]]
+			if not map.blocked(*next):
+				self.position[0] = next[0]
+				self.position[1] = next[1]
+			
+			self.percentage -= 1
 			self.changeDirection()
 	
 	def changeDirection(self):
-		self.direction[0] = self.nextDirection[0]
-		self.direction[1] = self.nextDirection[1]
+		self.direct[0] = self.nextDirection[0]
+		self.direct[1] = self.nextDirection[1]
 		
 	def setDirection(self, x):
 		self.nextDirection = x
@@ -84,12 +102,42 @@ class cMovingBody(cBody):
 class cPacMan(cMovingBody):
 	def __init__(self):
 		cMovingBody.__init__(self)
+		#super(cPacMan, self).__init__(self)
+		self.setColor(255, 255, 0)
+		self.startPoint = [0, 0]
+		
+		
+	def setStartPosition(self, *p):
+		self.setPosition(*p)
+		self.startPoint[0], self.startPoint[1] = p[0], p[1]
+		
 	
 	
 class cMonster(cMovingBody):
 	#path finding
 	def __init__(self):
 		cMovingBody.__init__(self)
+		
+	def pathFinding(self, _map):
+		x, y = self.position[0], self.position[1]
+		curValue = _map.getPFMvalue(*self.position)
+		posDirect = []
+		dir = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+		for i in dir:
+			newPos = [x + i[0], y + i[1]]
+			if _map.legalCheck(*newPos):
+				tmpValue = _map.getPFMvalue(*newPos)
+				if tmpValue < curValue:
+					posDirect.append(i)
+					
+			if _debug_:
+				print(i, tmpValue, curValue)
+		if posDirect:
+			nextMove = posDirect[int(random()*len(posDirect))]
+		else:
+			r = int(random() * 4)
+			nextMove = dir[r]
+		return nextMove
 		
 class cDeadMonster(cMovingBody):
 	pass
@@ -127,7 +175,7 @@ class cMap():
 					1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, \
 					1, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 1, \
 					1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, \
-					1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, \
+					1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 0, 1, \
 					1, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 1, \
 					1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, \
 					1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, \
@@ -135,10 +183,10 @@ class cMap():
 					1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, \
 					1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, \
 					]
-		self.pacman = None
+		self.pacman = cPacMan()
+		self.monsterGenerator = cMonsterGenerator()
 		self.monster = []
-		self.monsterGenerator = None
-		self.initializeMap()
+		self.pfm = [0 for i in range(len(self.map))]
 		
 	def initializeMap(self):
 		for i in range(len(self.map)):
@@ -147,39 +195,128 @@ class cMap():
 			elif self.map[i] == 1:
 				self.map[i] = cWall()
 			elif self.map[i] == 2:
-				if self.monsterGenerator is None:
-					self.monsterGenerator = cMonsterGenerator()
-				self.map[i] = self.monsterGenerator
+				self.map[i] = cBody()
+				self.monsterGenerator.setPosition(i%self.mapSize[0], int(i/self.mapSize[0]))
+			elif self.map[i] == 3: #start point
+				self.map[i] = cBody()
+				self.pacman.setStartPosition(i%self.mapSize[0], int(i/self.mapSize[0]))
 			else:
 				self.map[i] = cBody()
 			self.map[i].setPosition(i%self.mapSize[0], int(i/self.mapSize[0]))
+			
+			
+	def pathFindingMap(self):
+		p = self.pacman.getPosition()
+		# maxValue = self.mapSize[0] * self.mapSize[1]
+		# for i in range(len(self.map)):
+			# if (type(self.map[i]) == cWall):
+				# self.pfm[i] = maxValue
+			# else:
+				# self.pfm[i] = abs(i%self.mapSize[0] - p[0]) \
+							# + abs(i/self.mapSize[0] - p[1])
+							
+		self.dfsPathFinding(p[0], p[1], 0)
+							
+	def dfsPathFinding(self, x, y, dist):
+		if self.legalCheck(x, y) != True:
+			return
+			
+		t = y * self.mapSize[0] + x
+		maxValue = self.mapSize[0] * self.mapSize[1] + 1
 		
+		if type(self.map[t]) is cWall:
+			self.pfm[t] = maxValue
+			return
+		elif self.pfm[t] != 0 and self.pfm[t] < dist:
+			return
+			
+		self.pfm[t] = dist
+		
+		self.dfsPathFinding(x + 1, y, dist + 1)
+		self.dfsPathFinding(x - 1, y, dist + 1)
+		self.dfsPathFinding(x, y + 1, dist + 1)
+		self.dfsPathFinding(x, y - 1, dist + 1)
+		
+							
+	def getPFMvalue(self, x, y):
+		t = y * self.mapSize[0] + x
+		return self.pfm[t]
+				
 	
-class cController():
-	def __init__(self):
-		pass
+	def blocked(self, *position):
+		if position[0] >= self.mapSize[0] or position[0] < 0 \
+		or position[1] >= self.mapSize[1] or position[1] < 0:
+			return True
 		
-	def gameStart(self):
+		
+		tmp = position[0] + position[1] * self.mapSize[0]
+		if type(self.map[tmp]) == cWall:
+			return True
+			
+		if _debug_:
+			print(position[0], position[1], 'unblock, type ', type(self.map[tmp]))
+			
+		return False
+		
+	def legalCheck(self, *c):
+		if c[0] >= self.mapSize[0] or c[1] >= self.mapSize[1] or c[0] < 0 or c[1] < 0:
+			return False
+			
+		return True
+	
+	def warpmap(self, *c):
+		if c[0] >= self.mapSize[0]:
+			c[0] -= self.mapSize[0]
+			
+		if c[1] >= self.mapSize[1]:
+			c[1] -= self.mapSize[1]
+		
+		if c[0] < 0:
+			c[0] += self.mapSize[0]
+			
+		if c[1] < 0:
+			c[1] += self.mapSize[1]
+			
+		return c
+	
+class cCore():
+	def __init__(self):
 		self.map = cMap()
 		self.map.initializeMap()
-		self.map.pacman = cPacMan()
-		self.monsterGenerator = cMonsterGenerator()
-		sekf.initializeMap()
 		
+	def gameRun(self):
+		#generate monster
+		self.map.monsterGenerator.generateMonster(self.map.monster)
 		
+		#pathfinding
+		self.map.pathFindingMap()
+		for m in self.map.monster:
+			dir = m.pathFinding(self.map)
+			m.setDirection(dir)
 		
+		#move monster
+		for i in self.map.monster:
+			i.move(self.map)
+		
+		#move pacman
+		self.map.pacman.move(self.map)
+		
+		#death check
+		
+class cController():
+	pass	
 	
 class cGUI():
 	def __init__(self, screenSizeX, screenSizeY, background):
 		self.screenSize = (screenSizeX, screenSizeY)
-		self.pixalSize = 20
+		self.pixalSize = 40
 		self.mapSize = (int(self.screenSize[0]/self.pixalSize), int(self.screenSize[1]/self.pixalSize))
 		self.background = background
 		
 	def drawPacman(self, pac):
 		mapx, mapy = pac.getPosition()
-		center = (mapx * self.pixalSize, mapy * self.pixalSize)
-		radius = int(self.pixalSize * 0.45)
+		center = (mapx * self.pixalSize + self.pixalSize/2, mapy * self.pixalSize + self.pixalSize/2)
+		radius = int(self.pixalSize * 0.5)
 		
 		noStroke()
 		color = pac.getColor()	
@@ -203,13 +340,14 @@ class cGUI():
 	def drawMonster(self, monster):
 		mapx, mapy = monster.getPosition()
 		topLeft = (mapx * self.pixalSize, mapy * self.pixalSize)
-		rectSize = int(self.pixalSize * 0.6)
+		rectSize = [int(self.pixalSize * 0.6), int(self.pixalSize * 0.6)]
+		margin = int((self.pixalSize - rectSize[0])/2)
 		radius = int(self.pixalSize * 0.2)
 		
 		noStroke()
 		color = monster.getColor()
 		fill(*color)
-		rect(topLeft[0], topLeft[1], rectSize, rectSize, radius, radius, 0, 0)
+		rect(topLeft[0], topLeft[1], rectSize[0], rectSize[1], radius, radius, 0, 0)
 		
 	def drawWall(self, wall):
 		mapx, mapy = wall.getPosition()
@@ -227,14 +365,24 @@ class cGUI():
 		fill(*food.getColor())
 		ellipse(center[0], center[1], radius, radius)
 		
+	def drawNumber(self, body, map):
+		mapx, mapy = body.getPosition()
+		topLeft = (mapx * self.pixalSize, mapy * self.pixalSize)
+		textSize(20)
+		fill(255)
+		text(map.getPFMvalue(mapx, mapy), topLeft[0], topLeft[1])
+		
 	def default(self, obj):
 		if _debug_:
 			print("Enter default function, with type", type(obj))
 	
 	def drawMap(self, _map):
 		func = {cWall:self.drawWall, cFood:self.drawFood, cPacMan:self.drawPacman, cMonster:self.drawMonster}
-		for i in _map.map:
+		for i in _map.map + [_map.pacman] + _map.monster:
 			func.get(type(i), self.default)(i)
+			
+			if _debug_:
+				self.drawNumber(i, _map)
 		
 	
 game_state = 0 #0-title, 1-prepare, 2-ingame, 3-game end
@@ -244,56 +392,18 @@ playerNumber = 1
 conti = True
 
 
-gui = cGUI(600, 400, [0, 0, 0])
-pac = cPacMan()
-pac.setPosition(1, 1)
-pac.setColor(0, 255, 0)
-mon = cMonster()
-mon.setColor(255, 0, 0)
-mon.setPosition(2, 2)
-map = cMap()
-	 
+gui = cGUI(1000, 1000, [0, 0, 0])
+con = cCore()
+   
 def setup():
-	size(600, 450)
-	background(0, 0, 0)
+  size(1000, 1000)
+  background(0, 0, 0)
 
 
 def draw():
-	global map, game_state, pixel, playerNumber, conti, gui, pac, mon
-	#title
-	if game_state == 0:
-		background(0, 0, 0)
-		textSize(100)
-		fill(255, 255, 0)
-		text("PacMan", 140, 200)
-		textSize(20)
-		text("1.Single Player", 230, 270)
-		text("2.Double Player", 230, 300)
-	
-	elif game_state == 1:
-
-		
-		game_state = 2
-		
-	
-	elif game_state == 2:
-		background(0, 0, 0)
-		gui.drawMap(map)
-		
-	elif game_state == 3:
-		textSize(60)
-		fill(255, 0, 0)
-		text("GAME OVER", 120, 150)
-		textSize(30)
-	
-		score = []
-		for s in map.snakeList:
-			score.append(len(s.returnSnake()))
-		for i in range(len(score)):
-			text("Player %d: %d" %(i + 1, score[i] - 5), 200, 200 + 30 * i)
-		text("Press Space to return menu", 120, 350)
-	else:
-		game_state = 0
+  background(0, 0, 0)
+  con.gameRun()
+  gui.drawMap(con.map)  
 	
 def keyPressed():
 	global map, game_state, playerNumber
